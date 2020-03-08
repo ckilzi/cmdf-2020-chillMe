9e69b352f95fc2bd16e6cb9f2006b9eae6afa79f
ckilz <ckilzi@msn.com>
2020-03-07 20:19:54 -0800

    test

--- app/src/main/java/com/example/chillme/MainActivity.java
+++ app/src/main/java/com/example/chillme/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.chillme;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,27 +10,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button jokesButton;
-    private Button helpButton;
-    private Button breatheButton;
-    private Button musicButton;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
-
-       jokesButton=findViewById(R.id.jokesMenuButton);
-       breatheButton=findViewById(R.id.breathingMenuButton);
-       helpButton=findViewById(R.id.helpMenuButton);
-       musicButton=findViewById(R.id.musicMenuButton);
-
-       jokesButton.setOnClickListener(this);
-       breatheButton.setOnClickListener(this);
-       helpButton.setOnClickListener(this);
-       musicButton.setOnClickListener(this);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -41,22 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);*/
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.breathingMenuButton:
-                break;
-            case R.id.helpMenuButton:
-                break;
-            case R.id.musicMenuButton:
-                break;
-            case R.id.jokesMenuButton:
-                Intent intent= new Intent(MainActivity.this,JokesActivity.class);
-                startActivity(intent);
-                break;
-        }
-    }
 }
--- app/src/main/java/com/example/chillme/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/chillme/ui/home/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,7 +23,13 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
+        final TextView textView = root.findViewById(R.id.mainMenu);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
         return root;
     }
 }
